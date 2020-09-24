e99ca16a5d9e3645f8f654721d2a0c8214f9dd2d
s8013035 <yibuki.tsubasa@outlook.com>
2020-09-24 18:57:42 +0900

    SUN

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello Rct!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >KBDfans
           Learn React
         </a>
       </header>
